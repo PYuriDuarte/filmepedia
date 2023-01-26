c6cdcc893f98842a3f2c015e0e9c6d6971bf39bf
Yuri Duarte <116809586+PYuriDuarte@users.noreply.github.com>
2023-01-26 20:46:39 -0300

    commit style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,9 @@ const Navbar=()=>{
 
   return (    
     <nav id='navbar'>
-      <h2>
-        <Link to='/'><h1><BiCameraMovie/>Filmepédia</h1></Link>
-      </h2>
+      <h1>
+        <Link to='/'><BiCameraMovie/><p>Filmepédia</p></Link>
+      </h1>
       <form>
         <input type="text" placeholder='Busque um filme'/>
         <button type='submit'><BiSearchAlt2/></button>
