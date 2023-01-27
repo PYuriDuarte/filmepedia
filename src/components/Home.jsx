446e8cfd03c6708e79ec20ea3c47c0510d7b8490
Yuri Duarte <116809586+PYuriDuarte@users.noreply.github.com>
2023-01-27 16:09:19 -0300

    finalizado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,34 +1,35 @@
-import {useState, useEffect} from 'react';
-import MovieCard from './MovieCard';
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
 
-const moviesURL = import.meta.env.VITE_API
-const apiKey = import.meta.env.VITE_API_KEY
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
-const Home=()=>{
-
+const Home = () => {
   const [topMovies, setTopMovies] = useState([]);
 
-  const getTopRateMovies = async (url)=>{
+  const getTopRatedMovies = async (url) => {
     const res = await fetch(url);
-    const data = await res.json();    
+    const data = await res.json();
     setTopMovies(data.results);
-    console.log(topMovies);
   };
 
-  useEffect(()=>{
-    const topRateUrl=`${moviesURL}top_rated?${apiKey}`;
-    getTopRateMovies(topRateUrl);
-  },[]);
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    console.log(topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
+  }, []);
 
+  console.log(topMovies);
 
-  return(
-    <div className='container'>
-      <h2 className='title'>Melhores filmes</h2>
-      <div className='movies_container'>
-        {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie)=> <MovieCard key={movie.id} movie={movie}/>)}             
-      </div>      
+  return (
+    <div className="container">
+      <h2 className="title">Melhores filmes:</h2>
+      <div className="movies_container">
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
     </div>
   );
 };
+
 export default Home;
